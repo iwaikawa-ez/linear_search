190c28212dae57aa117e6f2a09c1d8b7d9e1e954
iwaikawa-ez <iwaikawa@exvisionz.jp>
2015-02-09 18:32:11 +0900

    インデント修正

--- exercise.php
+++ exercise.php
@@ -1,10 +1,10 @@
 <?php
-  require("linear.php");
-  if(linear_search($array, $search_num) == -1)
-  {
-    echo $search_num . "は見つかりませんでした";
-  }
-  else
-  {
-    echo $search_num . "が" . $i . "番目に見つかりました";
-  }
+require("linear.php");
+if(linear_search($array, $search_num) == -1)
+{
+  echo $search_num . "は見つかりませんでした";
+}
+else
+{
+  echo $search_num . "が" . $i . "番目に見つかりました";
+}
--- linear.php
+++ linear.php
@@ -1,17 +1,17 @@
 <?php
-  function linear_search($array, $search_num)
+function linear_search($array, $search_num)
+{
+  $cnt = count($array);
+  for($i = 0; $i < $cnt; $i++)
   {
-    $cnt = count($array);
-    for($i = 0; $i < $cnt; $i++)
+    if($array[$i] == $search_num)
     {
-      if($array[$i] == $search_num)
-      {
-        return $i;
-        break;
-      }
-    }
-    if(i >= $cnt)
-    {
-      return -1;
+      return $i;
+      break;
     }
   }
+  if(i >= $cnt)
+  {
+    return -1;
+  }
+}
